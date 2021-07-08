e288a12c445bfe995e4e412a95589428f89ae4d0
Gio Lodi <giovanni.lodi42@gmail.com>
2021-07-09 06:23:39 +1000

    Add source code for chapter 4

---
+++ 04-tdd-in-the-real-world/0-start/Albertos/AlbertosApp.swift
@@ -0,0 +1,10 @@
+import SwiftUI
+
+@main
+struct AlbertosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ 04-tdd-in-the-real-world/0-start/AlbertosUITests/AlbertosUITests.swift
@@ -0,0 +1,14 @@
+import XCTest
+
+class AlbertosUITests: XCTestCase {
+
+    func testExample() throws {
+        // Making the tests fail intentionally while there's no real test so we don't accidentally
+        // add them to the scheme that runs the unit tests only without noticing.
+        //
+        // Practicing Test-Driven Development is all about establishing as fast a feedback cycle as
+        // possible. Running tests that are slow to launch and on top of that do nothing is an
+        // unnecessary drag.
+        XCTFail("No test implemented yet!")
+    }
+}
