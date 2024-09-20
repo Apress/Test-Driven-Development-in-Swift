af6f63ed1d00ff5d805d021f7d8514aefc224c12
Gio Lodi <gio@mokacoding.com>
2024-09-20 18:16:45 +1000

    DRY `Makefile`

---
+++ constants.mk
@@ -0,0 +1,29 @@
+OS := 17.5
+DEVICE := iPhone 15
+APP_NAME := Albertos
+
+START_FOLDER := 0-start
+END_FOLDER := 1-end
+
+DESTINATION := OS=${OS},name=${DEVICE}
+
+SCHEME := ${APP_NAME}
+PROJECT := ${APP_NAME}.xcodeproj
+
+define xcodebuild_test
+	set -o pipefail && \
+	xcodebuild clean test \
+	-project $1/${PROJECT} \
+	-scheme ${SCHEME} \
+	-destination "${DESTINATION}" \
+	| xcbeautify
+endef
+
+define generate_project
+	xcodegen generate --spec $1/project.yml
+endef
+
+define run_tests
+	$(call generate_project, $1)
+	$(call xcodebuild_test, $1)
+endef
