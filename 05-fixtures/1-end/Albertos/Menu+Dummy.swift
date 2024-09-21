352c5f80f9c9fbdeda16f6ec2316feb2e4629c78
Gio Lodi <gio@mokacoding.com>
2024-09-22 06:44:48 +1000

    Further DRY by removing `Info.plist` from example 5 end

--- 05-fixtures/1-end/Albertos/Menu+Dummy.swift
+++ 05-fixtures/1-end/Albertos/Menu+Dummy.swift
@@ -1,19 +1,4 @@
-import SwiftUI
-
-@main
-struct AlbertosApp: App {
-
-    var body: some Scene {
-        WindowGroup {
-            NavigationView {
-                MenuList(sections: groupMenuByCategory(menu))
-                    .navigationTitle("Alberto's 🇮🇹")
-            }
-        }
-    }
-}
-
-// In this first iteration the menu is an hard-coded array
+// In this early iteration the menu is an hard-coded array
 let menu = [
     MenuItem(category: "starters", name: "Caprese Salad", spicy: false),
     MenuItem(category: "starters", name: "Arancini Balls", spicy: false),
